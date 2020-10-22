a962f0463cadb8f17dab57341b1ff7cdc4f6e7e6
Stefan Ziegler <edi.gonzales@gmail.com>
2020-10-22 17:01:48 +0200

    remove sql

--- sql/afu_gewaesserschutz_pub/postscript.sql
+++
@@ -1,8 +0,0 @@
-GRANT USAGE ON SCHEMA afu_gewaesserschutz_pub TO admin, gretl
-;
-GRANT SELECT ON ALL TABLES IN SCHEMA afu_gewaesserschutz_pub TO gretl
-;
-GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA afu_gewaesserschutz_pub TO admin
-;
-GRANT USAGE ON ALL SEQUENCES IN SCHEMA afu_gewaesserschutz_pub TO gretl, admin
-;
--- sql/arp_richtplan_pub/postscript.sql
+++
@@ -1,8 +0,0 @@
-GRANT USAGE ON SCHEMA arp_richtplan_pub TO admin, gretl
-;
-GRANT SELECT ON ALL TABLES IN SCHEMA arp_richtplan_pub TO gretl
-;
-GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA arp_richtplan_pub TO admin
-;
-GRANT USAGE ON ALL SEQUENCES IN SCHEMA arp_richtplan_pub TO gretl, admin
-;
